4a2860af4063bc53d9b27185f00218c43e0742df
magentaFire <dafne.magenta@gmail.com>
2017-04-03 17:17:06 -0500

    primer commit del ejercicio de hoy 3 de abril 2017 de propagation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var botonNuevoTweet = document.getElementById("botonNuevoTweet");
+
+botonNuevoTweet.addEventListener("click", mainFunction);
+
+function mainFunction(){
+  var nuevoTweet = document.getElementById("nuevoTweet");
+  var nombreTweeterx = document.getElementById("nombreTweeterx");
+  var contenedorNuevosTweets = document.getElementById("contenedorNuevosTweets");
+  alert("hjkl")
+  document.createElement("");
+
+}
